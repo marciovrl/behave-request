d0a1c159a8f20fc7db6eb4fde6dbbf2cab835135
marcio lemos <contato.marciovrl@gmail.com>
2020-04-02 10:22:36 -0300

    add create_token and refac create_post

--- features/helpers/data.py
+++ features/helpers/data.py
@@ -1,9 +1,17 @@
 from faker import Faker
+import os
 
 
-class Data:
-    def create_post(self):
-        return {
-            'titlle': Faker().word(ext_word_list=None),
-            'body': Faker().text(max_nb_chars=200, ext_word_list=None)
-        }
+def create_post():
+    ''' Returns a valid post payload '''
+
+    return {
+        'tittle': Faker().word(ext_word_list=None),
+        'body': Faker().text(max_nb_chars=200, ext_word_list=None)
+    }
+
+
+def create_token():
+    ''' Returns the value of the token that is in the environment variables '''
+
+    return os.getenv('TOKEN', 'var token does not exist')
